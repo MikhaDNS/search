7e17b8f9c5bf1166f1cc76fc44542992c2fabfa5
HKF15HKNFCFOR <66776729+HKF15HKNFCFOR@users.noreply.github.com>
2020-08-10 21:59:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -61,6 +61,7 @@ for(k;k<stringArray.length; k++){
 		}
 
 }
+document.getElementById("work_area").style.display='inline-block';
 document.getElementById("work_area").innerHTML +='<h1>Поиск завершен!</h1>';
 document.getElementById("one").innerHTML="Искать все ссылки"
 document.getElementById("one").style.background = "#2b995b";
@@ -127,6 +128,7 @@ if (index>0){
 }else{
 	alert('Нет доступных ссылок!')
 }
+document.getElementById("rnd_area").style.display='inline-block';
 document.getElementById("two").innerHTML="Случайная ссылка"
 document.getElementById("two").style.background = "#2b995b";
 }
