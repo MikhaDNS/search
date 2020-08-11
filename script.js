a868b055d17ab3689a041d4821b034efc9f68c66
HKF15HKNFCFOR <66776729+HKF15HKNFCFOR@users.noreply.github.com>
2020-08-11 10:17:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -99,8 +99,6 @@ for(k;k<stringArray.length; k++){
 		xml.send();
 		xml.onreadystatechange = function() { // (3)
 		  if (xml.readyState != 4) return;
-		document.getElementById("rnd_area").innerHTML ='<a target="_blank" href="' + element[randval] + '" ><h1> '+randval+'</h1></a>';
-		document.getElementById("rnd_area").style.display='inline-block';
 		document.getElementById("two").innerHTML="Случайная ссылка"
 		document.getElementById("two").style.background = "#2b995b";
 
@@ -117,6 +115,7 @@ for(k;k<stringArray.length; k++){
 				}
 				if (index>0){
 					let randval = Math.floor(Math.random() * index)	
+					document.getElementById("rnd_area").innerHTML ='<a target="_blank" href="' + element[randval] + '" ><h1> '+randval+'</h1></a>';
 				}
 		  }
 
@@ -126,6 +125,7 @@ for(k;k<stringArray.length; k++){
 	}
 
 }
+	document.getElementById("rnd_area").style.display='inline-block';
 }
 
 
