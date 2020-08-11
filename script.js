4afdcbdfaf06d159fc8e628bd2db6d43ea50ee91
HKF15HKNFCFOR <66776729+HKF15HKNFCFOR@users.noreply.github.com>
2020-08-12 00:29:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-let str='sitemap: hbggggg\n Sitemap: ghgghhhv'
-let rezf = str.match(/([Ss]itemap:\s)(.+)/g)
-alert(rezf)
-alert('hert')
 function isValidURL(string) {
   let res = string.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
   return (res !== null)
